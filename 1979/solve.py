7ec6fb6b94bc9d72b42fb6b9904fa875541556ed
최현우 <aduma489@gmail.com>
2024-07-25 01:48:08 +0900

    [SWEA] 1979 / d2 / 4h

--- 1979/solve.py
+++ 1979/solve.py
@@ -3,13 +3,28 @@ sys.stdin = open('input.txt')
 
 T = int(input())    # 테스트 케이스 갯수
 
-def three_words_repetition(lst, K):    # 테스트 케이스의 1개 열에 1이 K개 연속으로 나오는지 확인하는 함수            
-    for i in range(len(lst) - (K-1)):   
-        if lst[i] == lst[i+1] == lst[i+2]:
-            if i + K < len(lst) and lst[i+K] == 1:
-                return False
-            return True
-    return False
+def words_repetition(lst, K):    # 테스트 케이스의 1개 열에 1이 K개 연속으로 나오는지 확인하는 함수            
+    cnt = 0
+    sc = 0
+    # 리스트 요소가 1이면 cnt를 1 증가
+    # cnt가 K와 같다면(= 빈 공간이 단어 길이 만큼 있다) sc를 1 증가
+    # cnt가 K보다 크다면(= 빈 공간이 단어 길이보다 많다) sc를 1 감소, cnt를 0으로
+    # 리스트 요소가 1이 아니면 cnt는 0
+    
+    for i in range(len(lst)):   
+        if lst[i] == 1:
+            cnt += 1                  
+            if cnt == K:
+                sc += 1
+            elif cnt > K:
+                sc -= 1
+                cnt = 0
+        else:
+            cnt = 0
+    if lst == [1] * len(lst) and len(lst) % (K + 1) == 1:   # 예외 케이스: 요소가 1밖에 없어서 sc가 0이어야 하지만 1이 되는 경우
+        sc = 0
+    return sc
+
 
 for tc in range(1, T+1):
     success_case = 0
@@ -18,15 +33,14 @@ for tc in range(1, T+1):
 
     lst_group = []     # 테스트 케이스를 열별로 담을 리스트
     for _ in range(N):
-        
-        row = list(map(int, input().split()))   # 테스트 케이스의 한 열
-        lst_group.append(row)                   # 한 열을 리스트에 담기
+        row = list(map(int, input().split()))   # 테스트 케이스의 1개 열
+        lst_group.append(row)                   # 1개 열을 리스트에 담기
 
-        if three_words_repetition(row, K):         # K개의 빈 공간이 연속으로 있다면
-            success_case = success_case + 1     # 성공 케이스 숫자 + 1
-    for line in zip(*lst_group):                # 열을 행으로 변환
-        if three_words_repetition(line):
-            success_case = success_case + 1
+        sc = words_repetition(row, K)      # K개의 빈 공간이 연속으로 있다면
+        success_case = success_case + sc        # 성공 케이스 숫자 + 1
+    for line in zip(*lst_group):                # N개의 열을 담은 리스트를 행으로 변환
+        sc = words_repetition(line, K)       # K개의 빈 공간이 연속으로 있다면
+        success_case = success_case + sc        # 성공 케이스 숫자 + 1
     print(f'#{tc} {success_case}')
 
 '''
