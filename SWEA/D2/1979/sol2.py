abbb88e8bb5e45d53ccfa4e173ad0ac550f3dca3
최현우 <aduma489@gmail.com>
2024-08-17 20:45:52 +0900

    [SWEA] d2 / 어디에 단어가 들어갈 수 있을까

--- SWEA/D2/1979/sol2.py
+++ SWEA/D2/1979/sol2.py
@@ -7,19 +7,24 @@ for tc in range(1, T+1):
     N, K = list(map(int, input().split()))  # N: 퍼즐 크기 K: 단어 길이
     puzzle = [list(map(int, input().split())) for _ in range(N)]
 
-    start = []
+    result = 0
     for i in range(N):
+        row = col = 0
         for j in range(N):
-            if puzzle[0][j] == 1:
-                start += [[0, j]]
-            elif puzzle[i][0] == 1:
-                start += [[i, 0]]
-            elif puzzle[i-1][j] == 0 and puzzle[i][j] == 1:
-                start += [[i, j]]
-            elif puzzle[i][j-1] == 0 and puzzle[i][j] == 1:
-                start += [[i, j]]
-    print(start)
-
-    # for dx, dy in [0,1], [-1,0]:``
-
-
+            if puzzle[i][j] == 1:
+                row += 1
+            else:
+                if row == K:
+                    result += 1
+                row = 0
+            if puzzle[j][i] == 1:
+                col += 1
+            else:
+                if col == K:
+                    result += 1
+                col = 0
+        if row == K:
+            result += 1
+        if col == K:
+            result += 1
+    print(f'#{tc} {result}')
