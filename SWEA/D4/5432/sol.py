86e17059bfd434c674d3988b71262b5a74d009db
최현우 <aduma489@gmail.com>
2024-08-17 21:11:23 +0900

    [SWEA] d4 / 쇠막대기 자르기

--- SWEA/D4/5432/sol.py
+++ SWEA/D4/5432/sol.py
@@ -7,6 +7,7 @@ for tc in range(1, T+1):
     arr = list(map(str, input()))
     new_arr = []
 
+    # 레이저를 0으로 표시한 new_arr
     for k in range(len(arr)):
         if arr[k] == '(' and arr[k+1] == ')':
             new_arr += [0]
@@ -14,14 +15,20 @@ for tc in range(1, T+1):
             continue
         else:
             new_arr += arr[k]
-    print(new_arr)
 
 
-
-    sticks = [0] * 100
+    # 레이저가 막대기를 자르면 개수가 1개가 더 늘어남
     stack = []
-    point = 0
+    result = 0
 
-    for i in new_arr:
-        if i == '(':
+    # 스택에 ( 는 집어넣고 )가 나오면 pop, 레이저가 나오면 스택에 들어있는 수만큼 막대기 개수 추가
+    for x in new_arr:
+        if x == '(':
+            stack += [x]
+            result += 1
+        elif x == ')':
+            stack.pop()
+        else:
+            result += len(stack)
+    print(f'#{tc} {result}')
 
