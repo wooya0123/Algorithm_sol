dfbf6e97e47f34038f7ebb998935661440bea0f3
최현우 <aduma489@gmail.com>
2024-08-18 20:56:32 +0900

    [SWEA] d3 / 농작물 수확하기

--- SWEA/D3/2805/sol.py
+++ SWEA/D3/2805/sol.py
@@ -10,12 +10,12 @@ for tc in range(1, T+1):
     cnt = 0
 
     k = -1
-    for i in range(N//2, -1, -1):
-        k += 1
-        for j in range(k, N-k):
-            if k == 0:
+    for i in range(N//2, -1, -1):   # 행 조절
+        k += 1              # 열 조절하기 위한 k
+        for j in range(k, N-k):     # 열 범위
+            if k == 0:      # 중간 행일 경우 한 번 더하기
                 cnt += farm[i][j]
-            else:
+            else:           # 그 외 행일 경우 반대편도 더하기
                 cnt += farm[i][j]
                 cnt += farm[N-1-i][j]
     print(f'#{tc} {cnt}')
