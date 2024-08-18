8b1841719cec85fd4733d955f8dcb8fef9f9a44b
최현우 <aduma489@gmail.com>
2024-08-18 15:53:04 +0900

    [SWEA] d1 / 연속한 1의 개수

--- SWEA/D1/9386/sol.py
+++ SWEA/D1/9386/sol.py
@@ -26,7 +26,7 @@ for tc in range(1, T+1):
     cnt_one = 0
     while stack:        # 마지막이 1로 끝났을 때 스택 다 꺼내서 개수 세기기
        stack.pop()
-        cnt_one += 1
+       cnt_one += 1
     if result < cnt_one:
         result = cnt_one
 
