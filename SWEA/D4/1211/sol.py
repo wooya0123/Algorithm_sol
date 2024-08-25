ea2a4d74314eec04ddc056c1629e73f9cd1c5863
최현우 <aduma489@gmail.com>
2024-08-25 23:29:46 +0900

    [SWEA] d4 / Ladder2

---
+++ SWEA/D4/1211/sol.py
@@ -0,0 +1,38 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = 10
+
+for _ in range(1, 11):
+    tc = int(input())
+    arr = [list(map(int, input().split())) for _ in range(100)]
+
+    start = []
+    for j in range(100):
+        if arr[0][j] == 1:
+            start.append([0,j])
+
+    distance = 9999
+    result = 0
+    stack = []
+    for x,y in start:
+        visited = [[0] * 100 for _ in range(100)]
+        stack = [[x,y]]
+        visited[x][y] = 1
+        while stack:
+            x, y = stack.pop()
+            if x == 99:
+                if distance > visited[x][y] - 1:
+                    distance = visited[x][y] - 1
+                    result = y
+                elif distance == visited[x][y] - 1 and result < y:
+                    result = y
+
+            for dx, dy in [0,-1], [0,1], [1,0]:
+                nx = x + dx
+                ny = y + dy
+                if 0 <= nx < 100 and 0 <= ny < 100 and arr[nx][ny] != 0 and visited[nx][ny] == 0:
+                    stack.append([nx, ny])
+                    visited[nx][ny] = visited[x][y] + 1
+
+    print(result)
---
+++ SWEA/D4/1211/sol2.py
@@ -0,0 +1,49 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = 10
+
+for _ in range(1, 11):
+    tc = int(input())
+    arr = [list(map(int, input().split())) for _ in range(100)]
+
+    start = []
+    for j in range(100):
+        if arr[0][j] == 1:
+            start.append([0,j])
+
+    max_cnt = 0
+    X = 0
+    for x, y in start:
+        visited = [[0] * 100 for _ in range(100)]
+        visited[x][y] = 1
+        cnt = 0
+        while x < 99:
+            # 왼쪽에 1이 있는지
+            while 0 <= y:
+                if arr[x][y-1] == 1 and visited[x][y-1] == 0:
+                    y = y-1
+                    cnt += 1
+                    visited[x][y] = 1
+                else:
+                    break
+            # 오른쪽에 1이 있는지
+            while y < 99:
+                if arr[x][y+1] == 1 and visited[x][y+1] == 0:
+                    y = y+1
+                    cnt += 1
+                    visited[x][y] = 1
+                else:
+                    break
+            # 왼, 오에 1이 없으면 내려가기
+            if x < 98 and arr[x+1][y] != 0:
+                x += 1
+                cnt += 1
+                visited[x][y] = 1
+            else:
+                break
+
+        if x == 99 and max_cnt <= cnt:
+            max_cnt = cnt
+            X = y
+    print(max_cnt, X)
