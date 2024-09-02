14fe334c34c18d377da2cf6a6457e6db04338410
alpppano <aduma489@gmail.com>
2024-09-02 11:53:26 +0900

    [SWEA] d4 / 러시아 국기 같은 깃발

--- SWEA/D4/4613/sol.py
+++ SWEA/D4/4613/sol.py
@@ -1,24 +1,32 @@
 import sys
 sys.stdin = open('sample_input.txt')
 
+def check_rect(arr, x, y, color):
+    cnt = 0
+    for r in range(x, y):           # 행 범위 내에서 열 탐색하면서 color랑 같지 않으면 색칠
+        for c in range(M):
+            if arr[r][c] != color:
+                cnt += 1
+    return cnt
+
+
 T = int(input())
-
-for tc in range(1, T+1):
-    N, M = list(map(int, input().split()))      # N: 세로 M: 가로
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
     flag = [input() for _ in range(N)]
-    change_color = []
-    change_W = 0
-    change_B = 0
-    change_R = 0
 
-    for i in range(2, N):
-        W = B = R = 0
-        for j in range(len(flag)):
-            if flag[i][j] != 'W':
-                W += 1
-            if flag[i][j] != 'B':
-                B += 1
-            else:
-                R += 1
+    W = B = R = 0
+    ans = N * M                     # 최대 색칠 횟수
+    for i in range(1, N - 1):       # W 범위
+        for j in range(i + 1, N):   # B 범위
+            # 0, i
+            S = check_rect(flag, 0, i, 'W')
+            # i, j
+            S += check_rect(flag, i, j, 'B')
+            # j, N
+            S += check_rect(flag, j, N, 'R')
 
+            ans = min(ans, S)
+
+    print(f'#{tc} {ans}')
 
