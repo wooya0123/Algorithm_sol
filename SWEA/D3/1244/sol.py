1de6056fdc0352376d08524ef9a1edd78dc8434c
alpppano <aduma489@gmail.com>
2024-09-03 14:54:53 +0900

    [SWEA] d3 / 최대 상금

--- SWEA/D3/1244/sol.py
+++ SWEA/D3/1244/sol.py
@@ -1,35 +1,39 @@
-# import sys
-# sys.stdin = open('input.txt')
+import sys
+sys.stdin = open('input.txt')
 
-def card_change(cnt, lst):
-    if cnt == C:
+def card_change(cnt):
+    global answer
+    if cnt == C:                    # 카드 교환 횟수만큼 했다면 최댓값인지 확인
+        temp = int(''.join(card))
+        if answer < temp:
+            answer = temp
         return
 
-    if cnt >= len(lst)-1:                           # 1개 빼고 정렬됐을 때
-        card[-1], card[-2] = card[-2], card[-1]     # 맨 뒤에 2개만 계속 바꾸기
-        res = card_change(cnt+1, lst)
-    else:
-        lst = card[cnt:]
-        max_num = max(lst)
-        # 정렬할 리스트의 맨 앞에 최고 숫자가 아닐 때 뒤에서부터 탐색해서 최고 숫자를 맨 앞으로
-        if lst[0] != max_num:
-            for i in range(len(lst)-1, cnt-1, -1):
-                if card[i] == max_num and card[cnt] != card[i]:
-                    card[cnt], card[i] = card[i], card[cnt]
-                    res = card_change(cnt+1, card[cnt:])
-                    break
-        else:
-            new_lst = lst[1:]
-            card_change(cnt, new_lst)
-    return res
+    # 완전 탐색
+    # 앞 카드와 뒷 카드를 교환 -> 재귀를 통해 교환 횟수만큼 교환 -> visited에 해당 배열 저장
+    # 카드를 교환 후 확인하여 해당 배열을 확인한 적이 있다면 더 이상 보지 않음
+    for i in range(len(card)-1):
+        for j in range(i+1, len(card)):
+            card[i], card[j] = card[j], card[i]
 
+            current = ''.join(card)
+            if (cnt, current) in visited:
+                pass
+            else:
+                card_change(cnt + 1)
+                visited.append((cnt, current))
+
+            card[i], card[j] = card[j], card[i]
 
 T = int(input())
 
 for tc in range(1, T+1):
-    *card, change = input().split()    # card: 숫자카드 배열, change: 교환 횟수
-    card = list(map(int, card[0]))
-    C = int(change)
-    visited = [0] * len(card)
+    *card, C = input().split()    # card: 숫자카드 배열(str), C: 교환 횟수
+    card = list(card[0])
+    C = int(C)
+    visited = []
+    answer = 0
 
-    print(f'#{tc} {card_change(0, card)}')
+    card_change(0)
+
+    print(f'#{tc} {answer}')
