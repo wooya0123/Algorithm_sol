f595760739675c09218a185e932c6bcb9fb9c273
wooya0123 <aduma489@gmail.com>
2024-10-24 13:54:32 +0900

    [PGM] Lv2 / 석유 시추 / 효율성 개선 실패 풀이

--- PGM/Lv2/250136/sol2.py
+++ PGM/Lv2/250136/sol2.py
@@ -6,20 +6,22 @@ def solution(land):
     row = len(land)         # 행
     col = len(land[0])      # 열
 
+    oil = [[0, 0] * (col+1)]     # 열마다 시추한 석유 기록(idx: 열 번호, 값: 시추한 석유 덩어리 수)
     max_cnt = -1
     for j in range(col):    # 열 순회
-        n = j+1             # 석유 시추한 열을 표시
         i = 0               # 행 증가용
         cnt = 0             # 시추한 석유 덩어리 수
         while i < row:
-            if land[i][j] == 0:     # 해당 칸이 땅일 때
+            if land[i][j] == 0:             # 해당 칸이 땅일 때
                 i += 1
                 continue
-            if land[i][j] == n+1:   # 해당 칸이 이미 시추한 곳일 때
-                i += 1
-                continue
-            else:
-                land[i][j] = n+1    # 해당 칸 석유 시추
+            elif land[i][j] != 1:           # 해당 칸이 이미 시추한 곳일 때
+                if oil[j+1][1] == -1:
+                    cnt += oil[land[i][j] // 10][0]         # 바로 석유 수 더해주기
+                    oil[j+1][1] = land[i][j] // 10          # 참고한 열 기록
+
+            elif land[i][j] == 1:
+                land[i][j] = (j+1) * 10    # 해당 칸 석유 시추
                 cnt += 1
 
                 q = deque()
@@ -31,10 +33,11 @@ def solution(land):
                     for dx, dy in [-1,0], [1,0], [0,-1], [0,1]:
                         nx = x + dx
                         ny = y + dy
-                        if 0 <= nx < row and 0 <= ny < col and land[nx][ny] != 0 and land[nx][ny] <= n:
+                        if 0 <= nx < row and 0 <= ny < col and land[nx][ny] == 1:
                             q.append((nx, ny))
-                            land[nx][ny] = n + 1    # 해당 열에서는 시추했다고 표시
+                            land[nx][ny] = (j+1) * 10    # 해당 열에서는 시추했다고 표시
                             cnt += 1
+                oil[j+1][0] = cnt
                 i += 1
 
         if max_cnt < cnt:
