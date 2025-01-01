94ba66719dc0de95d2e6aacfb177f56cdb9a5def
최현우 <aduma489@gmail.com>
2025-01-02 01:43:23 +0900

    [BOJ] 2841 / s1 / 3h

---
+++ BOJ/1406/sol.py
@@ -0,0 +1,59 @@
+'''
+600,000 글자 입력 가능
+맨 처음에는 커서는 문장 맨 뒤에 위치
+모든 명령어 수행 후 편집기에 입력되어 있는 문자열 수
+문자열 길이: N
+영어 소문자로만
+입력할 명령어: M
+셋째 줄부터 M개의 줄에 걸쳐 명령어
+'''
+
+import sys
+from collections import deque
+
+sentence = sys.stdin.readline().rstrip()
+
+front = deque()
+back = deque()
+
+for i in range(len(sentence)):
+    front.append(sentence[i])
+M = int(sys.stdin.readline().rstrip())  # 명령어 개수
+# cursor = len(sentence)
+
+for i in range(M):
+    command = sys.stdin.readline().rstrip().split()
+
+    # 문자 입력 명령어
+    if len(command) == 2:
+        front.append(command[1])
+
+        # sentence = sentence[:cursor] + command[1] + sentence[cursor:]
+        # cursor += 1
+
+    # 삭제
+    elif command[0] == 'B':
+        if front:
+            front.pop()
+
+        # if cursor > 0:
+            # sentence = sentence[:cursor - 1] + sentence[cursor:]
+            # cursor -= 1
+
+    # 왼쪽 이동
+    elif command[0] == 'L':
+        if front:
+            word = front.pop()
+            back.appendleft(word)
+
+    # 오른쪽 이동
+    elif command[0] == 'D':
+        if back:
+            word = back.popleft()
+            front.append(word)
+
+res1 = ''.join(front)
+res2 = ''.join(back)
+res = res1 + res2
+
+print(res)
---
+++ BOJ/Silver1/2841/sol.py
@@ -0,0 +1,59 @@
+'''
+1~6번 줄, 1~p번 프랫(높은 수의 프랫의 음이 연주됨)
+프랫을 누르거나 떼면 손가락 한 번 움직인 거
+손가락 가장 적게 움직이는 방법
+N: 음의 수, 프랫의 수: P
+그 다음 N개의 줄 -> 줄 번호, 프랫 번호
+주어진 순서대로 연주
+'''
+import sys
+
+N, P = list(map(int, sys.stdin.readline().rstrip().split()))
+melody = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(N)]
+
+guitar = [[] for i in range(N+1)]     # 1~N번 줄
+cnt = 0
+
+for sound in melody:
+    string = sound[0]
+    fret = sound[1]
+    # 기타 줄에 프랫이 눌러져 있으면
+    if guitar[string]:
+        # 눌러야 하는 프렛이 눌러져 있는 프렛보다 크면 추가
+        if guitar[string][-1] < fret:
+            guitar[string].append(fret)
+            cnt += 1
+        # 같으면 그냥 넘어감
+        elif guitar[string][-1] == fret:
+            continue
+        # 작으면 눌러져 있는 프렛 하나씩 빼기
+        else:
+            while True:
+                if not guitar[string] or guitar[string][-1] < fret:
+                    guitar[string].append(fret)
+                    cnt += 1
+                    break
+                elif guitar[string][-1] > fret:
+                    guitar[string].pop()
+                    cnt += 1
+                    continue
+                elif guitar[string][-1] == fret:
+                    break
+
+
+    # 기타 줄에 프랫이 안 눌러져 있으면
+    else:
+        guitar[string].append(fret)
+        cnt += 1
+
+print(cnt)
+
+
+'''
+5 15
+2 8
+2 10
+2 12
+2 10
+2 5
+'''
