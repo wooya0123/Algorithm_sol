8c77e6df0734ef198b9997c051d1e0efd39de6e2
wooya0123 <aduma489@gmail.com>
2025-01-07 08:57:42 +0900

    [BOJ] 11722 가장 긴 감소하는 수열 / s2 / 1h

--- BOJ/Silver2/11722/sol.py
+++ BOJ/Silver2/11722/sol.py
@@ -1,3 +1,19 @@
+arr_len = int(input())
+arr = list(map(int, input().split()))
+
+# DP 배열 초기화 - 가장 짧은 수열의 길이가 1이므로 1로 초기화
+dp = [1] * arr_len
+
+# DP로 가장 긴 감소하는 부분 수열의 길이 계산
+for i in range(1, arr_len):
+    for j in range(i):
+        if arr[j] > arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+# 최대 길이 출력
+print(max(dp))
+
+# 이전 풀이 - 문제 이해를 잘못 함
 # arr_len = input()
 # arr = list(map(int, input().split()))
 # ans = []
@@ -13,18 +29,3 @@
 #         ans = res
 #
 # print(len(ans) if ans else 1)
-
-arr_len = int(input())
-arr = list(map(int, input().split()))
-
-# DP 배열 초기화
-dp = [1] * arr_len
-
-# DP로 가장 긴 감소하는 부분 수열의 길이 계산
-for i in range(1, arr_len):
-    for j in range(i):
-        if arr[j] > arr[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
-
-# 가장 긴 감소하는 부분 수열의 길이 출력
-print(max(dp))
