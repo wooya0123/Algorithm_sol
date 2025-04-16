1e531c26a0a91e8a29ac2482ff7671bd4d4050d8
wooya0123 <aduma489@gmail.com>
2025-04-16 09:11:35 +0900

    [BOJ] LCS 9251 / G5 / 1H / 최현우

--- BOJ/Gold5/9251/sol.py
+++ BOJ/Gold5/9251/sol.py
@@ -1,6 +1,12 @@
 str1 = input()
 str2 = input()
-dp = [[0] * (len(str1)+1) for _ in range(len(str2)+1)]
+dp = [[0] * (len(str2)+1) for _ in range(len(str1)+1)]
+# print(dp)
 
 for i in range(1, len(str1)+1):
     for j in range(1, len(str2)+1):
+        if str1[i-1] == str2[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+print(max(dp[len(str1)]))
